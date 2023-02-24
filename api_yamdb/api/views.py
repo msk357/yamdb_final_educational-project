29147a8136abca9dd7c2bb2ed40257cd7ad852a6
msk357 <108631219+msk357@users.noreply.github.com>
2023-02-24 15:19:28 +0300

    test3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -183,11 +183,12 @@ class TitleViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if self.action in ('retrieve', 'list'):
-            return Title.objects.select_related('category').\
-                prefetch_related('genre').\
-                annotate(rating=Avg('reviews__score'))
-        return Title.objects.select_related('category').\
-            prefetch_related('genre')
+            return Title.objects.select_related(
+                'category').prefetch_related(
+                'genre').annotate(rating=Avg(
+                'reviews__score'))
+        return Title.objects.select_related(
+            'category').prefetch_related('genre')
 
 
 class CategoryViewSet(ListCreateDestroy):
